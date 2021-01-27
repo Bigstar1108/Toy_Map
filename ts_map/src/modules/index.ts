5e710d1c7feecf61dea57ba1bba1ba770c2ebcbd
Daeseong Hwang <65744080+Bigstar1108@users.noreply.github.com>
2021-01-27 23:25:33 +0900

    [FIX]

--- ts_map/src/modules/index.ts
+++ ts_map/src/modules/index.ts
@@ -1,7 +1,7 @@
 import {combineReducers} from '@reduxjs/toolkit';
 
-import {Center} from './position/getCenter';
+import getCenter from './position/getCenter';
 
 export const rootReducer = combineReducers({
-    Center: Center.reducer
+    getCenter
 });
