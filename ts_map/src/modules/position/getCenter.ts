04ad550b8f78ebb229762dca75607927308caa1e
Daeseong Hwang <65744080+Bigstar1108@users.noreply.github.com>
2021-01-27 23:25:45 +0900

    [FIX]

--- ts_map/src/modules/position/getCenter.ts
+++ ts_map/src/modules/position/getCenter.ts
@@ -1,24 +1,36 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const getCenterPosition = () => {
-    const center = {lat: 0, lng: 0};
-
-    navigator.geolocation.getCurrentPosition((position) => {
-        center.lat = position.coords.latitude;
-        center.lng = position.coords.longitude;
-
-        console.log(`Your location is Here!\n-> lat: ${center.lat}, lng: ${center.lng} <-`);
-
-        return center;
-    });
-}
-
-export const Center = createSlice({
+const Center = createSlice({
     name: 'CenterReducer',
-    initialState: {},
+    initialState: {position: {lat: 0, lng: 0}, isLoading: false, error: false},
     reducers: {
-        getCenter: (state, action) => getCenterPosition(),
+        startGetCenter: state => {
+            state.isLoading = true;
+        },
+        hasError: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        },
+        getCenterSuccess: (state, action) => {
+            state.position.lat = action.payload.lat;
+            state.position.lng = action.payload.lng;
+            state.isLoading = false;
+        }
     }
 });
 
-export const {getCenter} = Center.actions;
+export default Center.reducer;
+
+const {startGetCenter, hasError, getCenterSuccess} = Center.actions;
+
+export const getCenterPosition = () => async (dispatch: any) => {
+    dispatch(startGetCenter());
+    try{
+        navigator.geolocation.getCurrentPosition((position) => {
+            const center = {lat: position.coords.latitude, lng: position.coords.longitude};
+            dispatch(getCenterSuccess(center));
+        })
+    }catch(e){
+        dispatch(hasError(e));
+    }
+}
