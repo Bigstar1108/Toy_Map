1350b5d72a71ea6a421664d2d27562ea2d589459
Dae Seong <daesung09854@gmail.com>
2021-01-28 16:09:27 +0900

    [FIX]

--- ts_map/src/modules/zone/zoneMenu.ts
+++ ts_map/src/modules/zone/zoneMenu.ts
@@ -4,14 +4,17 @@ const ZoneMenu = createSlice({
     name: 'ZoneMenuReducer',
     initialState: {zoneMenu: []},
     reducers: {
-        addZoneMenu: (state: any, action) => {
+        addZoneMenu: (state: any) => {
             const randomColor = Math.floor(Math.random() * 16777215).toString(16);
             state.zoneMenu.push({
                 id: Date.now(),
                 colorCode: randomColor
             });
         },
-        removeZoneMenu: (state:any, action) => state.zoneMenu.filter((list:any) => list.id !== action.payload),
+        removeZoneMenu: (state:any, action) => {
+            const result = state.zoneMenu.filter((list:any) => list.id !== action.payload);
+            state.zoneMenu = result;
+        },
     }
 });
 
