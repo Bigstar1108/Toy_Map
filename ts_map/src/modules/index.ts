f8e93038205c6ad3acaa2c1e85782af35be6c9cb
Dae Seong <daesung09854@gmail.com>
2021-01-28 19:57:24 +0900

    [FIX]
    fix combineReducer

--- ts_map/src/modules/index.ts
+++ ts_map/src/modules/index.ts
@@ -3,9 +3,11 @@ import {combineReducers} from '@reduxjs/toolkit';
 import getCenter from './position/getCenter';
 import zoneMenu from './zone/zoneMenu';
 import markerData from './zone/markerData';
+import zonePosition from './zone/zonePosition';
 
 export const rootReducer = combineReducers({
     getCenter,
     zoneMenu,
-    markerData
+    markerData,
+    zonePosition
 });
