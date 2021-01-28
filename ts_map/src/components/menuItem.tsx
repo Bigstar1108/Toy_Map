f943f2ed1406e718951dd660b2566dda5234494e
Dae Seong <daesung09854@gmail.com>
2021-01-28 19:59:17 +0900

    [FIX]

--- ts_map/src/components/menuItem.tsx
+++ ts_map/src/components/menuItem.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import {useDispatch, RootStateOrAny, useSelector} from 'react-redux';
 import {removeZoneMenu, setZoneIndex} from '../modules/zone/zoneMenu';
+import {clearMarkerData} from '../modules/zone/markerData';
+import {addZonePosition} from '../modules/zone/zonePosition';
 
 const MenuItemDiv = styled.div`
     display: flex;
@@ -17,15 +19,26 @@ const MenuItemDiv = styled.div`
 const MenuItem = (list: any) => {
     const dispatch = useDispatch();
     const {setIndex} = useSelector((state: RootStateOrAny) => state.zoneMenu);
+    const {markerData} = useSelector((state:RootStateOrAny) => state.markerData);
 
     const {id, colorCode} = list.list;
     const {index} = list;
+
+    const onClickMenuBtn = () => {
+        if(setIndex === id){
+            dispatch(clearMarkerData());
+            dispatch(setZoneIndex(id));
+        }else{
+            console.log("false");
+            dispatch(setZoneIndex(id));
+        }
+    }
 
     return(
         <MenuItemDiv>
             <span style = {{color: `#${colorCode}`}}>{`ZONE ${index + 1}`}</span>
             <>
-            <button disabled = {!(setIndex === undefined) && !(setIndex === id)} onClick = {() => dispatch(setZoneIndex(id))}>
+            <button disabled = {!(setIndex === undefined) && !(setIndex === id)} onClick = {onClickMenuBtn}>
                 {setIndex === id ? '설정 완료' : '설정 하기'}
             </button>
             <button onClick = {() => dispatch(removeZoneMenu(id))}>삭제</button>
